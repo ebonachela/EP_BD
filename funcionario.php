ea23f0b8115080b8228b1ce18e06c1af070c9d0f
Eduardo Bonachela <dudubonachela@gmail.com>
2021-12-15 12:17:40 -0300

    separa gerenciamento de funcionarios e pacientes

--- funcionario.php
+++ funcionario.php
@@ -13,82 +13,12 @@ session_start();
 
 
 <html>
-    <style>
-        span {
-            padding-right: 5px;
-        }
-        
-        table {
-            font-family: arial, sans-serif;
-            border-collapse: collapse;
-            width: 80%;
-            margin-left: auto;
-            margin-right: auto;
-        }
-
-        td, th {
-            border: 1px solid #dddddd;
-            text-align: left;
-            padding: 8px;
-        }
-
-        tr:nth-child(even) {
-            background-color: #dddddd;
-        }
-
-    </style>
     <body>
 
         <p>Funcionário</p>
 
-        <button onclick="window.location.href='cadastrar_funcionario.php'">Adicionar funcionário</button>
+        <button onclick="window.location.href='gerenciar_funcionarios.php'">Gerenciar funcionários</button>
+        <button onclick="window.location.href='gerenciar_pacientes.php'">Gerenciar pacientes</button>
         <button onclick="window.location.href='sair.php'">Sair</button>
-
-        <div>
-            <h3 style="text-align: center;">Lista de funcionários</h3>
-
-            <table>
-                <tr>
-                    <th>Nome</th>
-                    <th>RG</th>
-                    <th>Hora de início</th>
-                    <th>Salário</th>
-                    <th>CEP</th>
-                    <th>CNES</th>
-                    <th>Ação</th>
-                </tr>
-
-                <?php
-                    $resultado = mysqli_query($connect, "SELECT * FROM FUNCIONARIO ORDER BY NOME"); 
-
-                    if($resultado){
-                        while($linha = $resultado->fetch_assoc()) {
-
-                            echo '<tr>';
-                            echo '<td>'.$linha['NOME'].'</td>';
-                            echo '<td>'.$linha['RG'].'</td>';
-                            echo '<td>'.$linha['HORA_INICIO'].'</td>';
-                            echo '<td>R$'.$linha['SALARIO'].'</td>';
-                            echo '<td>'.$linha['CEP'].'</td>';
-                            echo '<td>'.$linha['CNES_ESTABELEC'].'</td>';
-                            
-                            echo 
-                                '<td>
-                                    <form action="cadastrar_funcionario.php" method="post" style="display: inline;">
-                                        <button name="atualizar" value="'.$linha['RG'].'" style="margin-right: 5px">Alterar</button>
-                                    </form>
-                                    <form action="remover_funcionario.php" method="post" style="display: inline;">
-                                        <button name="remover" value="'.$linha['RG'].'" style="margin-right: 5px">Remover</button>
-                                    </form>
-                                </td>';
-
-                            echo '</tr>';
-
-                        }
-                    }
-                ?>
-            </table>
-
-        </div>
     </body>
 </html>
