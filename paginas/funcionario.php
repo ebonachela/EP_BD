79f3af17dff323c3e66112f3e2a07904aabf8f2f
Eduardo Bonachela <dudubonachela@gmail.com>
2021-12-16 15:58:15 -0300

    estilo

--- paginas/funcionario.php
+++ paginas/funcionario.php
@@ -11,16 +11,33 @@ session_start();
 
 
 <html>
+    <style>
+
+    </style>
     <body>
 
-        <p>Funcionário</p>
+        <h2 style="text-align: center; margin-top: 20px">Página do Funcionário</h2>
 
-        <button onclick="window.location.href='../gerenciador/gerenciar_funcionarios.php'">Gerenciar funcionários</button>
-        <button onclick="window.location.href='../gerenciador/gerenciar_pacientes.php'">Gerenciar pacientes</button>
-        <button onclick="window.location.href='../gerenciador/gerenciar_estabelecimentos.php'">Gerenciar estabelecimentos</button>
-        <button onclick="window.location.href='../gerenciador/gerenciar_vacinas.php'">Gerenciar vacinas</button>
-        <button onclick="window.location.href='../gerenciador/gerenciar_doses.php'">Gerenciar aplicação</button>
-        <button onclick="window.location.href='../cadastro/aplicar_dose.php'">Aplicar dose</button>
-        <button onclick="window.location.href='../api/sair.php'">Sair</button>
+        <div>
+            <h3 style="text-align: center;">Gerenciar Setores</h3>
+            <div style="display: flex; justify-content: center;">
+                <button onclick="window.location.href='../gerenciador/gerenciar_funcionarios.php'">Gerenciar funcionários</button>
+                <button onclick="window.location.href='../gerenciador/gerenciar_pacientes.php'">Gerenciar pacientes</button>
+                <button onclick="window.location.href='../gerenciador/gerenciar_estabelecimentos.php'">Gerenciar estabelecimentos</button>
+                <button onclick="window.location.href='../gerenciador/gerenciar_vacinas.php'">Gerenciar vacinas</button>
+                <button onclick="window.location.href='../gerenciador/gerenciar_doses.php'">Gerenciar aplicação</button>
+            </div>
+        </div>
+
+        <div>
+            <h3 style="text-align: center;">Vacinação</h3>
+            <div style="display: flex; justify-content: center;">
+                <button onclick="window.location.href='../cadastro/aplicar_dose.php'">Aplicar dose</button>
+            </div>
+        </div>
+
+        <div style="display: flex; justify-content: center; margin-top: 30px;">
+            <button onclick="window.location.href='../api/sair.php'">Sair</button>
+        </div>
     </body>
 </html>
