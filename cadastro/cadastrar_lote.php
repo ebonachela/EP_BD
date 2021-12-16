052866c599abc8b374575af83e4dd1db130b2576
Eduardo Bonachela <dudubonachela@gmail.com>
2021-12-16 00:06:53 -0300

    adiciona lota

---
+++ cadastro/cadastrar_lote.php
@@ -0,0 +1,94 @@
+<?php
+
+$host = "localhost";
+$user = "root";
+$password = "";
+$db = "vacinacao";
+
+$connect = mysqli_connect($host, $user, $password, $db);
+mysqli_set_charset($connect,"utf8mb4");
+
+if(isset($_POST['CNPJ_FABRIC']) && !isset($_POST['atualizar'])){
+    $id_marca = $_POST['id_marca'];
+    $validade = $_POST['validade'];
+    $data_fabric = $_POST['data_fabric'];
+    $cnpj_fabric = $_POST['cnpj_fabric'];
+
+
+    if(!isset($_POST['update'])) {
+        $sql = "INSERT INTO LOTE VALUES('".$id_marca."', '".$validade."', '".$data_fabric."', '".$cnpj_fabric."');";
+    } else {
+        $id_lote = isset($_POST['id_lote']);
+        $sql = "UPDATE LOTE SET ID_MARCA = '".$id_marca."', VALIDADE = '".$validade."', DATA_FABRIC = '".$data_fabric."', CNPJ_FABRIC = '".$cnpj_fabric."' WHERE ID_LOTE = '".$id_lote."';";
+    }
+
+    if(mysqli_query($connect, $sql)){
+        echo " Cadastro feito com sucesso! ";
+        header("Location: ../gerenciador/gerenciar_vacinas.php"); 
+    } else {
+        echo " Erro ao realizar cadastro! ";
+        die();
+    }
+
+} else if(isset($_POST['atualizar'])){
+    $id_lote = $_POST['atualizar'];
+
+    $sql = "SELECT * FROM LOTE WHERE ID_LOTE = '".$id_lote."';";
+    
+    $result = mysqli_query($connect, $sql); 
+
+    if($result){
+        $dados = mysqli_fetch_array($result);
+        $id_marca = $dados['ID_MARCA'];
+        $validade = $dados['VALIDADE'];
+        $data_fabric = $dados['DATA_FABRIC'];
+        $cnpj_fabric = $dados['CNPJ_FABRIC'];
+    }
+}
+
+?>
+
+<html>
+    <body>
+
+        <p>Cadastrar Lote</p>
+
+        <form method="POST" action="#">
+            <label for="id_marca">ID Marca:</label><br>
+            <select name="id_marca" id="id_marca" style="width: 170px" <?php echo (isset($_POST['atualizar'])) ? 'value="'.$id_marca.'"' : ''; ?>>
+                <?php 
+                    $resultado = mysqli_query($connect, "SELECT * FROM marca ORDER BY ID_MARCA"); 
+
+                    if($resultado){
+                        while($linha = $resultado->fetch_assoc()) {
+                            echo '<option value="'.$linha['ID_MARCA'].'">'.$linha['ID_MARCA'].':'.$linha['NOME'].'</option>';
+                        }
+                    }
+                ?>
+            </select><br>
+
+            <label for="validade">Validade:</label><br>
+            <input type="text" id="validade" name="validade" <?php echo (isset($_POST['atualizar'])) ? 'value="'.$validade.'"' : ''; ?>><br>
+
+            <label for="data_fabric">Data Fabricação:</label><br>
+            <input type="text" id="data_fabric" name="data_fabric" <?php echo (isset($_POST['atualizar'])) ? 'value="'.$data_fabric.'"' : ''; ?>><br>
+
+            <label for="cnpj_fabric">Fabricante:</label><br>
+            <select name="cnpj_fabric" id="cnpj_fabric" style="width: 170px" <?php echo (isset($_POST['atualizar'])) ? 'value="'.$cnpj_fabric.'"' : ''; ?>>
+                <?php 
+                    $resultado = mysqli_query($connect, "SELECT * FROM FABRICANTE ORDER BY NOME"); 
+
+                    if($resultado){
+                        while($linha = $resultado->fetch_assoc()) {
+                            echo '<option value="'.$linha['CNPJ'].'">'.$linha['NOME'].'</option>';
+                        }
+                    }
+                ?>
+            </select><br>
+
+            <input type="submit" value="Enviar" <?php echo (isset($_POST['atualizar'])) ? 'name="update"' : ''; ?>>
+        </form>
+
+        <button onclick="window.location.href='../gerenciador/gerenciar_vacinas.php'">Voltar</button>
+    </body>
+</html>
--- gerenciador/gerenciar_vacinas.php
+++ gerenciador/gerenciar_vacinas.php
@@ -42,7 +42,8 @@ session_start();
 
         <p>Gerenciar Vacinas</p>
 
-        <button onclick="window.location.href='../cadastro/cadastrar_fabricantes.php'">Adicionar fabricantes</button>
+        <button onclick="window.location.href='../cadastro/cadastrar_fabricante.php'">Adicionar fabricantes</button>
+        <button onclick="window.location.href='../cadastro/cadastrar_lote.php'">Adicionar lotes</button>
         <button onclick="window.location.href='../paginas/funcionario.php'">Voltar</button>
 
         <div>
@@ -85,6 +86,50 @@ session_start();
             </table>
         </div>
 
-        
+        <div>
+            <h3 style="text-align: center;">Lista de lotes</h3>
+
+            <table>
+                <tr>
+                    <th>ID Lote</th>
+                    <th>ID Marca</th>
+                    <th>Fabricação</th>
+                    <th>Validade</th>
+                    <th>Nome do Fabricante</th>
+                    <th>Ação</th>
+                </tr>
+
+                <?php
+                    $resultado2 = mysqli_query($connect, "SELECT lote.ID_LOTE, lote.ID_MARCA, lote.VALIDADE, lote.DATA_FABRIC, lote.CNPJ_FABRIC, fabricante.NOME as NOME_FABRICANTE FROM lote INNER JOIN fabricante on fabricante.CNPJ = lote.CNPJ_FABRIC ORDER BY lote.ID_LOTE"); 
+
+                    if($resultado2){
+                        while($linha = $resultado2->fetch_assoc()) {
+
+                            echo '<tr>';
+                            echo '<td>'.$linha['ID_LOTE'].'</td>';
+                            echo '<td>'.$linha['ID_MARCA'].'</td>';
+                            echo '<td>'.$linha['DATA_FABRIC'].'</td>';
+                            echo '<td>'.$linha['VALIDADE'].'</td>';
+                            echo '<td>'.$linha['NOME_FABRICANTE'].'</td>';
+                            
+                            echo 
+                                '<td>
+                                    <form action="../cadastro/cadastrar_lote.php" method="post" style="display: inline;">
+                                        <button name="atualizar" value="'.$linha['ID_LOTE'].'" style="margin-right: 5px">Alterar</button>
+                                    </form>
+                                    <form action="../api/remover_lote.php" method="post" style="display: inline;">
+                                        <button name="remover" value="'.$linha['ID_LOTE'].'" style="margin-right: 5px">Remover</button>
+                                    </form>
+                                </td>';
+
+                            echo '</tr>';
+
+                        }
+                    }
+                ?>
+            </table>
+        </div>
+
+
     </body>
 </html>
